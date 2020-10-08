20b5ecb19ceebf577d2c69d892bac508a520da29
Andrew0502 <agcmarcus@gmail.com>
2020-10-07 22:14:06 -0400

    Built out schema.db and seeds.db. Added database name to connection.js.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slider', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
